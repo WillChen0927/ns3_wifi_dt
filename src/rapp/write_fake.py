34689e18ffd7ec00012af4a402db30d53c527a25
will <whale4878@gmail.com>
2025-07-10 10:19:07 +0800

    Modify sta config parameters

--- src/rapp/write_fake.py
+++ src/rapp/write_fake.py
@@ -22,7 +22,7 @@ def write_in_ap(ap_name, radio_band, channel, idle_time, channel_busy_time, tx_t
         Point("fake_ap_info")
         .tag("AP_Name", ap_name)
         .tag("RadioBand", radio_band)
-        .tag("Channel", channel)
+        .field("Channel", channel)
         .field("Idel time", idle_time)
         .field("Channel busy time", channel_busy_time)
         .field("TX time", tx_time)
@@ -45,8 +45,8 @@ def write_in_sta(sta_name, ssid, radio_band, ap_name, rx_bytes, tx_bytes, input_
         Point("fake_ap_info")
         .tag("STA_Name", sta_name)
         .tag("SSID", ssid)
-        .tag("RadioBand", radio_band)
-        .tag("AP_Name", ap_name)
+        .field("RadioBand", radio_band)
+        .field("AP_Name", ap_name)
         .field("RX_Bytes", rx_bytes)
         .field("Tx_Bytes", tx_bytes)
         .time(stored_time_str)
@@ -55,13 +55,13 @@ def write_in_sta(sta_name, ssid, radio_band, ap_name, rx_bytes, tx_bytes, input_
     write_api.write(bucket=bucket, org=org, record=point)
 
 
-write_in_sta("BMW_1F_AP01", "5GHz", "36", "95%", "2%", "1%", "2%", "100 kb/s", "200 kb/s", "2025-02-27T16:41:00Z")
-write_in_sta("BMW_1F_AP01", "2.4GHz", "1", "90%", "5%", "3%", "2%", "150 kb/s", "250 kb/s", "2025-02-27T16:42:00Z")
-write_in_sta("BMW_1F_AP02", "5GHz", "40", "92%", "3%", "2%", "3%", "120 kb/s", "220 kb/s", "2025-02-27T16:43:00Z")
+write_in_ap("BMW_1F_AP01", "5GHz", "36", "95%", "2%", "1%", "2%", "100 kb/s", "200 kb/s", "2025-02-27T16:41:00Z")
+write_in_ap("BMW_1F_AP01", "2.4GHz", "1", "90%", "5%", "3%", "2%", "150 kb/s", "250 kb/s", "2025-02-27T16:42:00Z")
+write_in_ap("BMW_1F_AP02", "5GHz", "40", "92%", "3%", "2%", "3%", "120 kb/s", "220 kb/s", "2025-02-27T16:43:00Z")
 
-write_in_ap("STA1", "bmwlab-peap", "5GHz", "BMW_1F_AP01", "500 kb/s", "600 kb/s", "2025-02-27T16:41:00Z")
-write_in_ap("STA2", "bmwlab-peap", "2.4GHz", "BMW_1F_AP01", "550 kb/s", "650 kb/s", "2025-02-27T16:42:00Z")
-write_in_ap("STA3", "bmwlab-peap", "5GHz", "BMW_1F_AP02", "520 kb/s", "620 kb/s", "2025-02-27T16:43:00Z")
+write_in_sta("STA1", "bmwlab-peap", "5GHz", "BMW_1F_AP01", "500 kb/s", "600 kb/s", "2025-02-27T16:41:00Z")
+write_in_sta("STA2", "bmwlab-peap", "2.4GHz", "BMW_1F_AP01", "550 kb/s", "650 kb/s", "2025-02-27T16:42:00Z")
+write_in_sta("STA3", "bmwlab-peap", "5GHz", "BMW_1F_AP02", "520 kb/s", "620 kb/s", "2025-02-27T16:43:00Z")
 
 
 print("DONE")
